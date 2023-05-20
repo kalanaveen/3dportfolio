98ff2d0a4db4cba2c13a4785c5a539880fb3a321
naveen kala <kalanaveen55@gmail.com>
2023-05-20 13:05:31 +0530

    3d model add

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,8 +18,8 @@ const Hero = () => {
             Hi, I'm <span className="text-[#915EFF]">Naveen</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop modern web applications <br className="sm:block hidden" />
-            and user interfaces
+          I develop 3D visuals, user <br className='sm:block hidden' />
+            interfaces and web applications
           </p>
         </div>
       </div>
